7ec3bfaac740f8eb2dcd8d4197fafabf02900d07
Max Mordini <Demorden@users.noreply.github.com>
2015-10-01 11:33:25 +0200

    Just pasting a working snippet
    Snippet created thanks to the help of Stanislav Khromov (WordPress Instant Articles co-author).
    https://wordpress.org/support/topic/per-page-prerendering-options-suggestion?replies=6
    to-do (maybe) get rid of ACF dependency

---
+++ prerendering-rules.php
@@ -0,0 +1,42 @@
+<?php
+/*
+* Plugin Name: Prerendering Rules by Max (Demorden)
+* Description: Snippet that allows override of prerendering rules. REQUIRES "WordPress Instant Articles" AND (for now) "Advanced Custom Fields". I could easily get rid of the latter but it's easier if you use it. Both are free anyway, and should be in your "must have" plugins list, imho.
+* Plugin URI:
+* Author: Max (Demorden)
+* Version: 1.0
+* Author URI: http://fantazine.eu/
+* License: GNU AGPL
+* Domain Path:       
+* Text Domain:       
+* GitHub Plugin URI: 
+* GitHub Branch:     master
+*/
+
+/*
+FIRST USE ACF TO CREATE A FIELD GROUP, NAME IT AS YOU LIKE, I SUGGEST SOMETHING ALONG THE LINES OF 'PRERENDERING RULES OVERRIDE' OR SOMETHING;
+CREATE POST OBJECT FIELD, IN MY EXAMPLE IT'S CALLED 'ARTICOLO_PRERENDER', AND A TRUE-FALSE FIELD ('OVERRIDE' IN MY EXAMPLE)
+CHOOSE FIELDS SETTINGS AS NEEDED, I WANT BOTH ACTIVE EVERYWHERE FOR ADMINS
+*/
+
+// FIRST we create ADDITIONAL rules: add selected posts to the list of prerendered posts in this page
+add_filter('wpinstant_prerendered_urls', function($urls) {
+    $values = get_field('articolo_prerender'); //change this according to your actual field name
+        if($values) {
+            foreach($values as $value) {
+                $permalink = get_permalink( $value );
+                if($permalink) {
+                    $urls[] = $permalink;
+                }
+            }
+        }
+return $urls;
+});
+
+// SECOND we IGNORE DEFAULT RULES (the ones set in the "WordPress Instant Articles" Settings Page), if -and only if- the override option is checked on page.
+// If we don't check the option, the plugin will prerender our hand-picked links AND ALSO its default ones.
+function($overresult) {
+	$override = get_field('override'); //change this according to your actual field name
+		if ( $override = 1 ) { add_filter('wpinstant_prerendered_urls_override_defaults', '__return_true'); }
+		else { add_filter('wpinstant_prerendered_urls_override_defaults', '__return_false'); }
+};
