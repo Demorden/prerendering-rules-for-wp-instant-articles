09c13ba8d5a2d696feae4cd5df1f7b65c7e182d2
Max Mordini <Demorden@users.noreply.github.com>
2015-10-01 15:05:23 +0200

    previous code not working properly
    now conditional overriding works properly with the true-false field;
    added conditions so that plugin does nothing without either ACF or WIA

--- prerendering-rules.php
+++ prerendering-rules.php
@@ -17,6 +17,10 @@ CREATE POST OBJECT FIELD, IN MY EXAMPLE IT'S CALLED 'ARTICOLO_PRERENDER', AND A 
 CHOOSE FIELDS SETTINGS AS NEEDED, I WANT BOTH ACTIVE EVERYWHERE FOR ADMINS
 */
 
+//function to check dependency
+function rewrite_prerendering_init() {
+	if( class_exists( 'WPInstantArticles_Plugin' ) && class_exists( 'acf' ) ) {
+
 // FIRST we create ADDITIONAL rules: add selected posts to the list of prerendered posts in this page
 add_filter('wpinstant_prerendered_urls', function($urls) {
     $values = get_field('articolo_prerender'); //change this according to your actual field name
@@ -33,8 +37,14 @@ return $urls;
 
 // SECOND we IGNORE DEFAULT RULES (the ones set in the "WordPress Instant Articles" Settings Page), if -and only if- the override option is checked on page.
 // If we don't check the option, the plugin will prerender our hand-picked links AND ALSO its default ones.
-function($overresult) {
+
+add_filter('wpinstant_prerendered_urls_override_defaults', function($overresult) {
 	$override = get_field('override'); //change this according to your actual field name
-		if ( $override = 1 ) { add_filter('wpinstant_prerendered_urls_override_defaults', '__return_true'); }
-		else { add_filter('wpinstant_prerendered_urls_override_defaults', '__return_false'); }
-};
+	if ( $override != 1 ) { return false; }
+	return true;
+});
+
+//close function for dependency
+}
+}
+add_action( 'plugins_loaded', 'rewrite_prerendering_init' );
